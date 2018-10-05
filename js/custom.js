8ba66916f655ce7266eef8b3af2f8dd96312c3eb
Gaut9a8m <gaut9a8m@gmail.com>
2018-10-05 22:00:46 +0530

    first add to my portfolio

---
+++ js/custom.js
@@ -0,0 +1,213 @@
+
+
+/* ----------------------------------------------------------------
+
+TABLE OF CONTENTS
+
+        + scrollIt
+        + close navbar-collapse when clicked
+        + navbar scrolling background
+        + progress bar
+        + magnificPopup
+        + Owl Carsouel
+            - clients carsouel
+            - blog carsouel
+        + stellar
+        + countUp
+        + Preloader
+        + isotope portfolio
+        + contact form validator
+        
+---------------------------------------------------------------- */
+
+$(function() {
+
+	"use strict";
+
+    var wind = $(window);
+
+
+    // scrollIt
+    $.scrollIt({
+      upKey: 38,                // key code to navigate to the next section
+      downKey: 40,              // key code to navigate to the previous section
+      easing: 'swing',         // the easing function for animation
+      scrollTime: 600,          // how long (in ms) the animation takes
+      activeClass: 'active',    // class given to the active nav element
+      onPageChange: null,       // function(pageIndex) that is called when page is changed
+      topOffset: -60            // offste (in px) for fixed top navigation
+    });
+
+
+    // close navbar-collapse when a  clicked
+    $(".nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("in").addClass("collapse");
+    });
+
+
+    // navbar scrolling background
+    wind.on("scroll",function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar-default")
+
+        if(bodyScroll > 300){
+
+            navbar.addClass("nav-scroll");
+
+        }else{
+
+            navbar.removeClass("nav-scroll");
+        }
+    });
+
+
+    // progress bar
+    wind.on('scroll', function () {
+        $(".skills-progress span").each(function () {
+            var bottom_of_object = 
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = 
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    });
+
+
+    // magnificPopup
+    $('.portfolio .link').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+            enabled: true
+        }
+    });
+
+
+    //// === owl carsouel === /////
+
+    // clients carsouel
+    $('.clients .owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        smartSpeed:500
+    });
+
+
+   
+
+	// stellar
+    wind.stellar();
+
+
+    // countUp
+    $('.numbers .counter').countUp({
+        delay: 10,
+        time: 1500
+    });
+    
+
+});
+
+$(window).on("load",function (){
+
+    // Preloader
+    $(".loading").addClass("loading-end").fadeOut(1000);
+
+
+    // isotope
+    $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+
+
+    // contact form
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    });
+
+});
+
+var hands = [];
+hands.push(document.querySelector('#secondhand > *'));
+hands.push(document.querySelector('#minutehand > *'));
+hands.push(document.querySelector('#hourhand > *'));
+
+var cx = 100;
+var cy = 100;
+
+function shifter(val) {
+  return [val, cx, cy].join(' ');
+}
+
+var date = new Date();
+var hoursAngle = 360 * date.getHours() / 12 + date.getMinutes() / 2;
+var minuteAngle = 360 * date.getMinutes() / 60;
+var secAngle = 360 * date.getSeconds() / 60;
+
+hands[0].setAttribute('from', shifter(secAngle));
+hands[0].setAttribute('to', shifter(secAngle + 360));
+hands[1].setAttribute('from', shifter(minuteAngle));
+hands[1].setAttribute('to', shifter(minuteAngle + 360));
+hands[2].setAttribute('from', shifter(hoursAngle));
+hands[2].setAttribute('to', shifter(hoursAngle + 360));
+
+for(var i = 1; i <= 12; i++) {
+  var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  el.setAttribute('x1', '100');
+  el.setAttribute('y1', '30');
+  el.setAttribute('x2', '100');
+  el.setAttribute('y2', '40');
+  el.setAttribute('transform', 'rotate(' + (i*360/12) + ' 100 100)');
+  el.setAttribute('style', 'stroke: #ffffff;');
+  document.querySelector('svg').appendChild(el);
+}
